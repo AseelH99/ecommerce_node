d313283406ae434c5a7ad18a2afe4bd9407fdc2b
work <aseel99basel@gmail.com>
2023-10-28 19:53:07 +0100

    create user,category model

--- src/modules/categories/cacegories.controller.js
+++ src/modules/categories/cacegories.controller.js
@@ -1,4 +1,18 @@
+import slugify from "slugify";
+import cloudinary from "../../services/cloudinary.js";
+import categoryModel from "../../../DB/model/categories.model.js";
 export const getGategories= (req,res)=>{
     return res.json("Welcome to Categories page");
 
+}
+export const creatCategory = async(req,res)=>{
+    const name = req.body.name.toLowerCase();
+    if(await categoryModel.findOne({name})){
+        return res.status(409).json({"message":"category name is allready exisit"});
+    }
+    const {secure_url,public_id}= await cloudinary.uploader.upload(req.file.path,{
+        folder:`${process.env.APPNAME}/categories`
+    })
+     const cat =await categoryModel.create({name,slug:slugify(name),image:{secure_url,public_id}});
+    res.status(201).json({"message":"category is created",cat});
 }
--- src/modules/categories/categories.router.js
+++ src/modules/categories/categories.router.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import * as categoriesRouter from './cacegories.controller.js'
+import fileUpload, { fileValidation } from "../../services/multer.js";
 const router= Router();
 router.get('/',categoriesRouter.getGategories)
+router.post('/',fileUpload(fileValidation.image).single('image'),categoriesRouter.creatCategory)
 export default router;
